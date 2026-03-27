44f794845ced1a2c5b0d39d97e233bb621d7cd82
Azamat-JS <azamatielts987@gmail.com>
2026-03-27 16:54:37 +0500

    Started full-stack project

---
+++ apps/backend/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT;
+  await app.listen(PORT ?? 3000, () => {
+    console.log('server is running on port ' + PORT)
+  });
+}
+bootstrap();
