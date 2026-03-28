873dc12e6744ec1ce45d063eb37ea3b32df648db
Azamat-JS <azamatielts987@gmail.com>
2026-03-28 23:03:37 +0500

    implemented chatgateway

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,15 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ChatModule } from './chat/chat.module';
 
 
 @Module({
   imports: [
     ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
-    PrismaModule
+    PrismaModule,
+    ChatModule
   ],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule { }
