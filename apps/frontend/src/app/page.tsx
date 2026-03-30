0dc3a7ad06fe1a7525d16c9c02f8f42b52122975
Azamat-JS <azamatielts987@gmail.com>
2026-03-31 03:40:59 +0500

    implemented header and dark & light mode

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import { CardDemo } from "@/components/AuthForm";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <Header />
       <h1 className="text-4xl font-bold text-center mt-10">Welcome to the Full Stack Turbo App!</h1>
       <p className="text-center mt-4 text-lg">This is the frontend of your full stack application.</p>
       <div className="flex flex-col gap-2 justify-center mt-10">
---
+++ apps/frontend/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { ModeToggle } from "./mode-toggle"
+
+export const Header = () => {
+    return (
+        <div className="w-full h-16 flex items-center justify-between mt-5 px-10">
+            <h1 className="text-2xl font-bold">My App</h1>
+            <div className="flex gap-4 text-xl">
+                <p className="hover:text-gray-400 cursor-pointer">Products</p>
+                <p className="hover:text-gray-400 cursor-pointer">About</p>
+                <p className="hover:text-gray-400 cursor-pointer">Contact</p>
+                <p className="hover:text-gray-400 cursor-pointer">Cart</p>
+            </div>
+            <ModeToggle />
+        </div>
+    )
+}
+
+export default Header
