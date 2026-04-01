611c529838b6a24ac5598130bd8439a9c864c5ca
Azamat-JS <azamatielts987@gmail.com>
2026-04-01 16:02:53 +0500

    created AuthRepository

--- apps/mobile/lib/features/auth/domain/repositories/auth_repository.dart
+++ apps/mobile/lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,1 +1,20 @@
-abstract interface class AuthRepository {}
+import 'package:fpdart/fpdart.dart';
+import 'package:mobile/core/errors/failures.dart';
+import 'package:mobile/features/auth/domain/entities/user_entity.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, UserEntity>> signUpWithEmailAndPassword({
+    required String email,
+    required String password,
+    required String username,
+  });
+
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  });
+
+  Future<Either<Failure, UserEntity>> getCurrentUser();
+
+  Future<Either<Failure, void>> signOut();
+}
