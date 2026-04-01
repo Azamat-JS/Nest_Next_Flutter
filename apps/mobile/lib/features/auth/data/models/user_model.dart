3b42931294adb7de7efa2988c4cc3107ca534efd
Azamat-JS <azamatielts987@gmail.com>
2026-04-01 17:17:36 +0500

    created user model

---
+++ apps/mobile/lib/features/auth/data/models/user_model.dart
@@ -0,0 +1,13 @@
+import 'package:mobile/features/auth/domain/entities/user_entity.dart';
+
+class UserModel extends UserEntity {
+  UserModel({required super.id, required super.username, required super.email});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      id: json['id'] as String,
+      username: json['username'] as String,
+      email: json['email'] as String,
+    );
+  }
+}
