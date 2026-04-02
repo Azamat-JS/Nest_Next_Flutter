ecae3288f8b8c6de49fe897e07e1689d44dffe3a
Azamat-JS <azamatielts987@gmail.com>
2026-04-02 16:01:03 +0500

    implemented app theme for text field, and scaffold bg colors

--- apps/mobile/lib/core/theme/app_theme.dart
+++ apps/mobile/lib/core/theme/app_theme.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/core/theme/app_pallete.dart';
 
 class AppTheme {
-  static final ThemeData darkThemeMode = ThemeData.dark().copyWith();
+  static OutlineInputBorder _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
+    borderRadius: BorderRadius.circular(10),
+    borderSide: BorderSide(color: color, width: 3),
+  );
+  static final darkThemeMode = ThemeData.dark().copyWith(
+    scaffoldBackgroundColor: AppPallete.backgroundColor,
+    appBarTheme: const AppBarTheme(backgroundColor: AppPallete.backgroundColor),
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: EdgeInsets.all(27),
+      enabledBorder: _border(),
+      focusedBorder: _border(AppPallete.gradient2),
+      errorBorder: _border(AppPallete.errorColor),
+    )
+  );
 }
