0277135881e257fc768de7d2c462347b1e1e88a1
Azamat-JS <azamatielts987@gmail.com>
2026-04-03 20:09:29 +0500

    completed auth event and auth state logic

--- apps/mobile/lib/features/auth/presentation/bloc/auth_bloc.dart
+++ apps/mobile/lib/features/auth/presentation/bloc/auth_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/features/auth/domain/entities/user_entity.dart';
 part 'auth_event.dart';
 part 'auth_state.dart';
 
--- apps/mobile/lib/features/auth/presentation/bloc/auth_event.dart
+++ apps/mobile/lib/features/auth/presentation/bloc/auth_event.dart
@@ -2,3 +2,26 @@ part of 'auth_bloc.dart';
 
 @immutable
 sealed class AuthEvent {}
+
+final class AuthSignUp extends AuthEvent {
+  final String username;
+  final String email;
+  final String password;
+
+  AuthSignUp({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+}
+
+final class AuthLogin extends AuthEvent {
+  final String email;
+  final String password;
+
+  AuthLogin({required this.email, required this.password});
+}
+
+final class AuthLoadCurrentUser extends AuthEvent {}
+
+final class AuthLogout extends AuthEvent {}
