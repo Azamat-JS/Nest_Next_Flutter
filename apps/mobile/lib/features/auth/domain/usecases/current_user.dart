f48acb44d1a96890eb3a483c6cdbdaa87f5482ce
Azamat-JS <azamatielts987@gmail.com>
2026-04-04 14:26:17 +0500

    implemented current user usecase

---
+++ apps/mobile/lib/features/auth/domain/usecases/current_user.dart
@@ -0,0 +1,15 @@
+import 'package:fpdart/src/either.dart';
+import 'package:mobile/core/common/entities/user_entity.dart';
+import 'package:mobile/core/errors/failures.dart';
+import 'package:mobile/core/usecase/usecase.dart';
+import 'package:mobile/features/auth/domain/repositories/auth_repository.dart';
+
+class CurrentUser implements Usecase<UserEntity, NoParams> {
+  final AuthRepository repository;
+  const CurrentUser(this.repository);
+
+  @override
+  Future<Either<Failure, UserEntity>> call(NoParams params) async {
+    return await repository.getCurrentUser();
+  }
+}
--- apps/mobile/lib/init_dependencies.dart
+++ apps/mobile/lib/init_dependencies.dart
@@ -3,6 +3,10 @@ import 'package:get_it/get_it.dart';
 import 'package:mobile/core/common/cubit/auth_check_cubit.dart';
 import 'package:mobile/core/network/dio_client.dart';
 import 'package:mobile/features/auth/data/datasourses/auth_remote_data_source.dart';
+import 'package:mobile/features/auth/data/repositories/auth_repository_iml.dart';
+import 'package:mobile/features/auth/domain/repositories/auth_repository.dart';
+import 'package:mobile/features/auth/domain/usecases/user_login.dart';
+import 'package:mobile/features/auth/domain/usecases/user_sign_up.dart';
 
 final serviceLocator = GetIt.instance;
 
@@ -15,10 +19,16 @@ Future<void> initDependencies() async {
 }
 
 void _initAuth() {
-  serviceLocator.registerLazySingleton<AuthRemoteDataSource>(
-    () => AuthRemoteDataSourceImpl(
-      serviceLocator<DioClient>(),
-      serviceLocator<FlutterSecureStorage>(),
-    ),
-  );
+  serviceLocator
+    ..registerLazySingleton<AuthRemoteDataSource>(
+      () => AuthRemoteDataSourceImpl(
+        serviceLocator<DioClient>(),
+        serviceLocator<FlutterSecureStorage>(),
+      ),
+    )
+    ..registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryIml(serviceLocator<AuthRemoteDataSource>()),
+    )
+    ..registerLazySingleton<UserSignUp>(() => UserSignUp(serviceLocator<AuthRepository>()))
+    ..registerLazySingleton<UserLogin>(() => UserLogin(serviceLocator<AuthRepository>()))
 }
