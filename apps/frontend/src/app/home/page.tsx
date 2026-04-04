048376fddc7eb1bc7e80f15284344b5e48d50166
Azamat-JS <azamatielts987@gmail.com>
2026-04-04 19:11:42 +0500

    added modal in home page

--- apps/frontend/src/app/home/page.tsx
+++ apps/frontend/src/app/home/page.tsx
@@ -26,6 +26,20 @@ import {
     DropdownMenuShortcut,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Edit, Menu, Trash } from "lucide-react"
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Field, FieldGroup } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
 const HomePage = () => {
     const token = useAuthStore((state) => state.token);
@@ -76,18 +90,79 @@ const HomePage = () => {
                             <TableCell className={user?.username === u.username ? "font-medium bg-green-300 text-green-600" : ""}>
                                 <DropdownMenu>
                                     <DropdownMenuTrigger>
-                                        <Button variant="outline">Open</Button>
+                                        <Button variant="outline">
+                                            <Menu />
+                                        </Button>
                                     </DropdownMenuTrigger>
                                     <DropdownMenuContent className="w-40" align="start">
                                         <DropdownMenuGroup>
                                             <DropdownMenuLabel>My Account</DropdownMenuLabel>
                                             <DropdownMenuItem>
-                                                Profile
-                                                <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+                                                <Dialog>
+                                                    <form>
+                                                        <DialogTrigger>
+                                                            <Button variant="outline"><Edit /></Button>
+                                                        </DialogTrigger>
+                                                        <DialogContent className="sm:max-w-sm">
+                                                            <DialogHeader>
+                                                                <DialogTitle>Edit profile</DialogTitle>
+                                                                <DialogDescription>
+                                                                    Make changes to your profile here. Click save when you&apos;re
+                                                                    done.
+                                                                </DialogDescription>
+                                                            </DialogHeader>
+                                                            <FieldGroup>
+                                                                <Field>
+                                                                    <Label htmlFor="name-1">Name</Label>
+                                                                    <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                                                                </Field>
+                                                                <Field>
+                                                                    <Label htmlFor="username-1">Username</Label>
+                                                                    <Input id="username-1" name="username" defaultValue="@peduarte" />
+                                                                </Field>
+                                                            </FieldGroup>
+                                                            <DialogFooter>
+                                                                <DialogClose>
+                                                                    <Button variant="outline">Cancel</Button>
+                                                                </DialogClose>
+                                                                <Button type="submit">Save changes</Button>
+                                                            </DialogFooter>
+                                                        </DialogContent>
+                                                    </form>
+                                                </Dialog>
                                             </DropdownMenuItem>
                                             <DropdownMenuItem>
-                                                Billing
-                                                <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+                                                <Dialog>
+                                                    <form>
+                                                        <DialogTrigger>
+                                                            <Button variant="outline"><Trash /></Button>
+                                                        </DialogTrigger>
+                                                        <DialogContent className="sm:max-w-sm">
+                                                            <DialogHeader>
+                                                                <DialogTitle>Delete profile</DialogTitle>
+                                                                <DialogDescription>
+                                                                    Are you sure to delete this user?
+                                                                </DialogDescription>
+                                                            </DialogHeader>
+                                                            <FieldGroup>
+                                                                <Field>
+                                                                    <Label htmlFor="name-1">Username</Label>
+                                                                    <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                                                                </Field>
+                                                                <Field>
+                                                                    <Label htmlFor="username-1">Email</Label>
+                                                                    <Input id="username-1" name="username" defaultValue="@peduarte" />
+                                                                </Field>
+                                                            </FieldGroup>
+                                                            <DialogFooter>
+                                                                <DialogClose>
+                                                                    <Button variant="outline">Cancel</Button>
+                                                                </DialogClose>
+                                                                <Button type="submit" variant="destructive">Delete</Button>
+                                                            </DialogFooter>
+                                                        </DialogContent>
+                                                    </form>
+                                                </Dialog>
                                             </DropdownMenuItem>
                                         </DropdownMenuGroup>
                                     </DropdownMenuContent>
