7f244617795db48403baaba83b3de779e96aaece
Azamat-JS <azamatielts987@gmail.com>
2026-04-09 20:24:36 +0500

    implemented API call in group details section

--- apps/frontend/src/app/(pages)/groups/[groupId]/page.tsx
+++ apps/frontend/src/app/(pages)/groups/[groupId]/page.tsx
@@ -1,8 +1,8 @@
 import GroupDetailsComponent from "@/components/pageContents/GroupDetailsComponent";
 
 const GroupDetails = async ({ params }: { params: Promise<{ groupId: string }> }) => {
-    const groupId = await (await params).groupId;
-    return <GroupDetailsComponent productId={groupId} />
+    const groupId = (await params).groupId;
+    return <GroupDetailsComponent groupId={groupId} />
 }
 
 export default GroupDetails
--- apps/frontend/src/components/pageContents/GroupDetailsComponent.tsx
+++ apps/frontend/src/components/pageContents/GroupDetailsComponent.tsx
@@ -1,10 +1,31 @@
 'use client'
+import { useAuthStore } from '@/lib/stores/authStore';
+import { GroupType } from '@/lib/types/groups';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { toast } from 'sonner';
+
+const GroupDetailsComponent = ({ groupId }: { groupId: string }) => {
+    const [group, setGroup] = useState<GroupType>();
+    const token = useAuthStore((state) => state.token);
+    const API = process.env.NEXT_PUBLIC_API_URL;
 
 
-const GroupDetailsComponent = ({ productId }: { productId: string }) => {
-    console.log(productId)
+    const getOneGroup = async () => {
+        try {
+            const res = await axios.get(`${API}/group/${groupId}`, { headers: { Authorization: `Bearer ${token}` } })
+        } catch (error: any) {
+            toast.error(error.response?.data?.message ?? 'Something went wrong!')
+        }
+    }
+
+    useEffect(() => {
+        getOneGroup()
+    }, [groupId, token])
     return (
-        <div>GroupDetailsComponent {productId}</div>
+        <div>
+
+        </div>
     )
 }
 
