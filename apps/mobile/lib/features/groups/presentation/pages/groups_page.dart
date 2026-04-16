3f7309faa637f71d91e5409fd4f6430c39dfb760
Azamat-JS <azamatielts987@gmail.com>
2026-04-16 14:48:30 +0500

    implemented search input

--- apps/mobile/lib/features/groups/presentation/pages/groups_page.dart
+++ apps/mobile/lib/features/groups/presentation/pages/groups_page.dart
@@ -10,6 +10,52 @@ class GroupsPage extends StatefulWidget {
 class _GroupsPageState extends State<GroupsPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Group page')));
+    return Scaffold(
+      appBar: AppBar(title: const Text('Group page')),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [_buildHeader(context)],
+        ),
+      ),
+    );
   }
+}
+
+Widget _buildHeader(BuildContext context) {
+  return Container(
+    alignment: Alignment.center,
+    decoration: BoxDecoration(
+      color: Colors.blue,
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: const InputDecoration(
+                  hintText: 'Search your groups...',
+                  border: InputBorder.none,
+                  suffixIcon: Icon(Icons.search, size: 30),
+                  focusColor: Colors.lightBlue,
+                  hintStyle: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
+}
+
+Widget _buildBody(BuildContext context) {
+  return Container();
+}
+
+Widget _buildFooter(BuildContext context) {
+  return Container();
 }
