ee501dfdad3eb023449c96486584578de1837078
Azamat-JS <azamatielts987@gmail.com>
2026-04-18 14:12:40 +0500

    updated student card removed bloc

--- apps/mobile/lib/features/groups/presentation/widgets/student_card.dart
+++ apps/mobile/lib/features/groups/presentation/widgets/student_card.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mobile/core/common/entities/user_entity.dart';
-import 'package:mobile/core/utils/show_snackbar.dart';
-import 'package:mobile/features/groups/presentation/bloc/group_bloc.dart';
 
 class StudentCard extends StatelessWidget {
   final UserEntity student;
@@ -23,37 +20,22 @@ class StudentCard extends StatelessWidget {
 }
 
 Widget _cardBody(BuildContext context, UserEntity student) {
-  return BlocConsumer<GroupBloc, GroupState>(
-    listener: (context, state) {
-      if (state.failure != null) {
-        showSnackbar(context, state.failure!.message);
-      }
-    },
-    builder: (context, state) {
-      if (state.isLoading) {
-        return const Center(child: CircularProgressIndicator());
-      }
-      if (state.failure != null) {
-        return Center(child: Text('Error occurred'));
-      }
-      return Column(
-        children: [
-          CircleAvatar(maxRadius: 40, backgroundColor: Colors.blue),
-          const SizedBox(height: 10),
-          Text(
-            student.username,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 25,
-              color: Colors.lightBlue,
-            ),
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-          const SizedBox(height: 10),
-          Text(student.email, style: const TextStyle(fontSize: 16)),
-        ],
-      );
-    },
+  return Column(
+    children: [
+      CircleAvatar(maxRadius: 40, backgroundColor: Colors.blue),
+      const SizedBox(height: 10),
+      Text(
+        student.username,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25,
+          color: Colors.lightBlue,
+        ),
+        maxLines: 2,
+        overflow: TextOverflow.ellipsis,
+      ),
+      const SizedBox(height: 10),
+      Text(student.email, style: const TextStyle(fontSize: 16)),
+    ],
   );
 }
