50207b65e908dd83848b502f77e39677098b0387
Azamat-JS <azamatielts987@gmail.com>
2026-04-20 16:03:35 +0500

    created Error handler

--- apps/frontend/src/app/(pages)/groups/[groupId]/page.tsx
+++ apps/frontend/src/app/(pages)/groups/[groupId]/page.tsx
@@ -1,13 +1,17 @@
 import GroupDetailsComponent from "@/components/pageContents/GroupDetailsComponent";
-import Spinner from "@/components/Spinner";
+import Spinner from "@/components/utils/Spinner";
+import { ErrorBoundary } from "react-error-boundary"
 import { Suspense } from "react";
+import ErrorHandler from "@/components/utils/ErrorHandler";
 
 const GroupDetails = async ({ params }: { params: Promise<{ groupId: string }> }) => {
     const groupId = (await params).groupId;
     return (
-        <Suspense fallback={<Spinner />}>
-            <GroupDetailsComponent groupId={groupId} />
-        </Suspense>
+        <ErrorBoundary fallback={<ErrorHandler />}>
+            <Suspense fallback={<Spinner />}>
+                <GroupDetailsComponent groupId={groupId} />
+            </Suspense>
+        </ErrorBoundary>
     )
 }
 
---
+++ apps/frontend/src/components/utils/ErrorHandler.tsx
@@ -0,0 +1,10 @@
+
+const ErrorHandler = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-2xl font-bold mb-4">An error occurred</h1>
+        </div>
+    )
+}
+
+export default ErrorHandler
