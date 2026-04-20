1bbd449b1520ce4e1b865b1f0763aab02320279c
Azamat-JS <azamatielts987@gmail.com>
2026-04-20 15:18:43 +0500

    updated find total score of student function

--- apps/backend/src/student_score/student_score.service.ts
+++ apps/backend/src/student_score/student_score.service.ts
@@ -75,17 +75,6 @@ export class StudentScoreRepository {
     }
 
     async findTotalScoreByStudentAndGroup(studentId: string, groupId: string) {
-        const totalScore = this.prisma.studentScore.findUnique({
-            where: {
-                studentId_groupId: {
-                    studentId,
-                    groupId,
-                },
-            },
-            select: {
-                total: true,
-            }
-        });
 
         const startOfDay = new Date();
         startOfDay.setHours(0, 0, 0, 0);
@@ -93,22 +82,34 @@ export class StudentScoreRepository {
         const endOfDay = new Date();
         endOfDay.setHours(23, 59, 59, 999);
 
-        const todayScoreEvents = this.prisma.scoreEvent.findMany({
-            where: {
-                studentId,
-                groupId,
-                createdAt: {
-                    gte: startOfDay,
-                    lte: endOfDay,
+        const [total, todayEvents] = await Promise.all([
+            this.prisma.studentScore.findUnique({
+                where: {
+                    studentId_groupId: {
+                        studentId,
+                        groupId,
+                    },
+                },
+                select: {
+                    total: true,
                 }
-            },
-            select: {
-                type: true,
-                value: true,
-            }
-        });
+            }),
 
-        const [total, todayEvents] = await this.prisma.$transaction([totalScore, todayScoreEvents]);
+            this.prisma.scoreEvent.findMany({
+                where: {
+                    studentId,
+                    groupId,
+                    createdAt: {
+                        gte: startOfDay,
+                        lte: endOfDay,
+                    }
+                },
+                select: {
+                    type: true,
+                    value: true,
+                }
+            }),
+        ]);
 
         return {
             total: total?.total || 0,
