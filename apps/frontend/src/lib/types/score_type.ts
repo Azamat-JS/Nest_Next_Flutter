403911b07f477d9783ac37e87c2edb5589fbf075
Azamat-JS <azamatielts987@gmail.com>
2026-04-20 16:17:56 +0500

    created GroupStudentScore type

--- apps/frontend/src/lib/types/score_type.ts
+++ apps/frontend/src/lib/types/score_type.ts
@@ -1,1 +1,13 @@
 export type ScoreType = 'HOMEWORK' | 'ATTENDANCE';
+
+export type GroupStudentScore = {
+    studentId: string;
+    type: ScoreType;
+    value: number;
+    createdAt: Date;
+    student: StudentUsername;
+}
+
+type StudentUsername = {
+    username: string;
+}
