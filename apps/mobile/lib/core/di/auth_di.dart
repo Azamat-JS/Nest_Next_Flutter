854e266c73b9dbd56efd86a68923e678a42c55d0
Azamat-JS <azamatielts987@gmail.com>
2026-04-22 18:45:15 +0500

    fixed bug in auth_di

--- apps/mobile/lib/core/di/auth_di.dart
+++ apps/mobile/lib/core/di/auth_di.dart
@@ -1,7 +1,5 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:mobile/core/common/cubit/auth_check_cubit.dart';
 import 'package:mobile/core/di/service_locator.dart';
-import 'package:mobile/core/network/dio_client.dart';
 import 'package:mobile/features/auth/data/datasourses/auth_remote_data_source.dart';
 import 'package:mobile/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:mobile/features/auth/domain/repositories/auth_repository.dart';
@@ -13,12 +11,6 @@ import 'package:mobile/features/auth/presentation/bloc/auth_bloc.dart';
 
 Future<void> initAuth() async {
   serviceLocator
-    ..registerLazySingleton<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl(
-        serviceLocator<DioClient>(),
-        serviceLocator<FlutterSecureStorage>(),
-      ),
-    )
     ..registerLazySingleton<AuthRepository>(
       () => AuthRepositoryImpl(serviceLocator<AuthRemoteDataSource>()),
     )
