245584738784696dce3f730843d5cc9b8f440c14
Azamat-JS <azamatielts987@gmail.com>
2026-04-29 17:43:22 +0500

    updated go router

--- apps/mobile/lib/core/router/app_router.dart
+++ apps/mobile/lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mobile/core/common/cubit/auth_check_cubit.dart';
+import 'package:mobile/core/common/widgets/auth_gate.dart';
 import 'package:mobile/core/di/service_locator.dart';
 import 'package:mobile/core/router/go_refresh_notifier.dart';
 import 'package:mobile/features/auth/presentation/pages/login_page.dart';
@@ -13,35 +14,11 @@ import 'package:mobile/features/student_scores/presentation/bloc/student_score_b
 import 'package:mobile/features/student_scores/presentation/pages/student_scores_page.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: '/groups',
-  refreshListenable: GoRouterRefreshNotifier(
-    serviceLocator<AuthCheckCubit>().stream,
-  ),
-  redirect: (context, state) {
-    final authState = context.read<AuthCheckCubit>().state;
-
-    final isAuthChecking = authState is AuthChecking;
-    final isLoggedIn = authState is AuthUserLoggedIn;
-
-    if (isAuthChecking) return null;
-
-    final loggingIn = state.matchedLocation == '/login';
-    final signUp = state.matchedLocation == '/signup';
-
-    if (!loggingIn && !signUp && !isLoggedIn) {
-      return '/login';
-    }
-
-    if (signUp && isLoggedIn && loggingIn) {
-      return '/groups';
-    }
-
-    if (loggingIn && isLoggedIn) {
-      return '/groups';
-    }
-    return null;
-  },
+  initialLocation: '/',
   routes: [
+    // Auth Gate
+    GoRoute(path: '/', builder: (_, state) => const AuthGate()),
+
     /// Group List
     GoRoute(path: '/groups', builder: (context, state) => const MyGroupsPage()),
 
