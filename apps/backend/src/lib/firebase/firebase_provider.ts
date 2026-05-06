5f22b40f30879c020195458becda5b157ba85e4f
Azamat-JS <azamatielts987@gmail.com>
2026-05-06 17:10:27 +0500

    updated firebase provider

--- apps/backend/src/lib/firebase/firebase_provider.ts
+++ apps/backend/src/lib/firebase/firebase_provider.ts
@@ -7,12 +7,13 @@ export const FirebaseAdminProvider: Provider = {
     provide: 'FIREBASE_ADMIN',
     inject: [AppConfig],
     useFactory: (config: AppConfig) => {
-        return admin.initializeApp({
+        const firebaseApp = admin.apps.length > 0 ? admin.app() : admin.initializeApp({
             credential: admin.credential.cert({
                 projectId: config.FIREBASE_PROJECT_ID,
                 clientEmail: config.FIREBASE_CLIENT_EMAIL,
                 privateKey: config.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
             })
         })
+        return firebaseApp;
     }
 }
