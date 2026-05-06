09a5c14901ee8406bd4bf9ecf8f093f3b3a40716
Azamat-JS <azamatielts987@gmail.com>
2026-05-06 16:45:57 +0500

    updated notification service

--- apps/backend/src/lib/shared/services/notification.service.ts
+++ apps/backend/src/lib/shared/services/notification.service.ts
@@ -1,17 +1,27 @@
 import { Inject, Injectable } from "@nestjs/common";
 import * as admin from 'firebase-admin';
+import { ScoreCreatedEvent } from "src/lib/events/score_create_event";
 
 @Injectable()
 export class NotificationService {
     constructor(@Inject('FIREBASE_ADMIN') private firebaseApp: admin.app.App) { }
 
-    async sendNotification(token: string) {
+    async sendNotification(event: ScoreCreatedEvent) {
+        const token = await this.getParentDeviceToken(event.studentId);
         await this.firebaseApp.messaging().send({
             token,
             notification: {
-                title: 'Text',
-                body: 'Hi, this is from backend'
+                title: 'New Score Added',
+                body: `Your child received ${event.score} for ${event.type} on ${event.date}`
+            },
+            data: {
+                studentId: event.studentId,
+                type: event.type
             }
-        })
+        });
+    }
+
+    async getParentDeviceToken(studentId: string): Promise<string> {
+        return 'fake token';
     }
 }
--- apps/backend/src/student_score/student_score_event_listener.ts
+++ apps/backend/src/student_score/student_score_event_listener.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@nestjs/common";
 import { OnEvent } from "@nestjs/event-emitter"
 import { ScoreCreatedEvent } from "src/lib/events/score_create_event";
+import { NotificationService } from "src/lib/shared/services/notification.service";
 
 @Injectable()
 export class StudentScoreEventListener {
-    constructor(private notificationService) { }
+    constructor(private notificationService: NotificationService) { }
 
     @OnEvent('score.created')
     async handleScoreCreated(event: ScoreCreatedEvent) {
-        await this.notificationService.sendScoreNotification(event)
+        await this.notificationService.sendNotification(event)
     }
 }
