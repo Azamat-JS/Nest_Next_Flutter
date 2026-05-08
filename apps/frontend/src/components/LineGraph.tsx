c46a547cda57694588e925d89b66a15e032f54b3
Azamat-JS <azamatielts987@gmail.com>
2026-05-08 17:41:36 +0500

    implemented date picker in linegraph comp

--- apps/frontend/src/components/LineGraph.tsx
+++ apps/frontend/src/components/LineGraph.tsx
@@ -1,7 +1,44 @@
+'use client'
+
+import React, { useState } from "react"
+import { format } from "date-fns"
+import { ChevronDownIcon } from "lucide-react"
+import { Button } from "@/components/ui/button"
+import { Calendar } from "@/components/ui/calendar"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
 
 const LineGraph = () => {
+    const [month, setMonth] = useState("January");
+    const [year, setYear] = useState("2026");
+    const [date, setDate] = React.useState<Date>()
+
     return (
-        <div>LineGraph</div>
+        <div>
+            <Popover>
+                <PopoverTrigger asChild>
+                    <Button
+                        variant="outline"
+                        data-empty={!date}
+                        className="w-53 justify-between text-left font-normal data-[empty=true]:text-muted-foreground"
+                    >
+                        {date ? format(date, "PPP") : <span>Pick a date</span>}
+                        <ChevronDownIcon />
+                    </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        defaultMonth={date}
+                    />
+                </PopoverContent>
+            </Popover>
+        </div>
     )
 }
 
