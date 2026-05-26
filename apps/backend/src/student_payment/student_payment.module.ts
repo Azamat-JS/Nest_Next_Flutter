b2496fdeb2c6c8d33b16f740991fefa57a6936f1
Azamat-JS <azamatielts987@gmail.com>
2026-05-26 19:59:09 +0500

    fixed bug in student payment module

--- apps/backend/src/student_payment/student_payment.module.ts
+++ apps/backend/src/student_payment/student_payment.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { StudentPaymentService } from './student_payment.service';
 import { StudentPaymentController } from './student_payment.controller';
+import { ConfigifyModule } from '@itgorillaz/configify';
+import { JwtModule } from '@nestjs/jwt';
+import { AppConfig } from 'src/lib/config';
 
 @Module({
+  imports: [
+    ConfigifyModule.forRootAsync(),
+    JwtModule.registerAsync({
+      inject: [AppConfig],
+      useFactory: (config: AppConfig) => ({
+        secret: config.JWT_SECRET,
+        signOptions: { expiresIn: config.JWT_EXPIRES_IN },
+      })
+    }),
+  ],
   controllers: [StudentPaymentController],
   providers: [StudentPaymentService],
 })
-export class StudentPaymentModule {}
+export class StudentPaymentModule { }
