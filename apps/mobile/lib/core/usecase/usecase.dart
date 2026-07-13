23b51fb5d32b9378511c9407aefb6bffdd7d27f2
Azamat-JS <azamatielts987@gmail.com>
2026-07-13 15:53:13 +0500

    removed mobile from root

--- apps/mobile/lib/core/usecase/usecase.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:mobile/core/errors/failures.dart';
-
-abstract interface class Usecase<SuccessType, Params> {
-  Future<Either<Failure, SuccessType>> call(Params params);
-}
-
-class NoParams {}
--- apps/mobile/lib/features/groups/presentation/bloc/recent_group/bloc/recent_group_event.dart
+++
@@ -1,6 +0,0 @@
-part of 'recent_group_bloc.dart';
-
-@immutable
-sealed class RecentGroupEvent {}
-
-final class LoadRecentGroups extends RecentGroupEvent {}
